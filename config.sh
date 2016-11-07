da8f48b3135a21766c3fab0841f2cabbc9dce0f1
Jonathan <mail@strfry.org>
2016-11-07 18:07:46 +0100

    Work on config menu

--- config.sh
+++ config.sh
@@ -2,21 +2,37 @@
 
 mkdir -p config
 
-dialog --menu "EWindow Configuration Menu" \
+CHOICE=$(dialog --menu "EWindow Configuration Menu" \
 	20 100 10 \
+	"IP" "Enter IP Address Number" \
+	"Hostname" "" \
+	"Description" "Change long description of this EWindow instance" \
 	"WiFi" "Enter Network Configuration" \
+	"Update" "Pull the newest version of this Software" \
+	3>&1 1>&2 2>&3 )
+
+case $CHOICE in
+  Update)
+    git pull
+    ;;
+  *)
+    echo "Not Implemented"
+    exit 23
+    ;;
+esac
 
 
-dialog --inputbox "WiFi SSID" 10 40 "$(cat config/wifissid)" 2>config/wifissid
+function do_wifi() {
+  dialog --inputbox "WiFi SSID" 10 40 "$(cat config/wifissid)" 2>config/wifissid
+  dialog --inputbox "Enter Wifi Password (leave empty for open networks)" 10 40 "$(cat config/wifipsk)" 2> config/wifipsk
+  SSID_LINE="ssid=\"$(cat config/wifissid)\""
+  PSK_LINE="key_mgmt=NONE"
 
-dialog --inputbox "Enter Wifi Password (leave empty for open networks)" 10 40 "$(cat config/wifipsk)" 2> config/wifipsk
+  if [ -n "$(cat config/wifipsk)" ]; then
+    PSK_LINE="psk=\"$(cat config/wifipsk)\""
+  fi
 
-SSID_LINE="ssid=\"$(cat config/wifissid)\""
-PSK_LINE="key_mgmt=NONE"
+  echo -e "network={\n" "$SSID_LINE" "\n" "$PSK_LINE" "\n}"
+  # TODO: Write it somewhere relevant
+}
 
-if [ -n "$(cat config/wifipsk)" ]; then
-  PSK_LINE="psk=\"$(cat config/wifipsk)\""
-fi
-
-
-echo -e "network={\n" "$SSID_LINE" "\n" "$PSK_LINE" "\n}"
