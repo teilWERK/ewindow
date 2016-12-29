ea4cac5577d5d15b3a89eb8d2b2eede30b93bf2e
Jonathan <mail@ewindow.org>
2016-12-29 13:19:21 +0000

    XXX shit from exil.ewindow.org XXX

--- menu/main.sh
+++ menu/main.sh
@@ -12,6 +12,7 @@ CHOICE=$(dialog --menu "$(hostname)" 20 100 10 \
 	Audioloop "Hear Local Microphone (Echo)" \
 	Shell "OS Shell" \
 	Info "Display debug information" \
+	Shutdown "poweroff" \
 	Exit "" \
 	3>&1 1>&2 2>&3 )
 
@@ -45,6 +46,9 @@ case $CHOICE in
 		;;
 	Info)
 		./info.sh
+		;;
+	Shutdown)
+		poweroff
 		;;
 	*)
 		exit
