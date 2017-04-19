2e7d6995ce90e39992d6ac15132f6259d623eaa0
JonathanS <mail@strfry.org>
2017-04-19 16:19:28 +0000

    Fixed Ewindow user Setup script

--- setup-ewindow-user.sh
+++ setup-ewindow-user.sh
@@ -12,16 +12,18 @@ fi
 
 systemctl set-default multi-user.target
 ln -sf /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+
+# Autologin with EWindow user
 sed -i -e "s/--autologin pi/--autologin ewindow/g" /etc/systemd/system/autologin@.service
 
-# Replace pi with ewindow user
-
-useradd -G video,audio,netdev -s /home/ewindow/login.sh ewindow
-git clone https://github.com/strfry/ewindow /home/ewindow
+#useradd -G video,audio,netdev -s /home/ewindow/login.sh ewindow
 chown -R ewindow:ewindow /home/ewindow/
+
+# Set up daemontools autorun:
+
+apt install -y daemontools-run
 
 ln -s /home/ewindow/configs/peervpn /etc/service/
 ln -s /home/ewindow/configs/baresip /etc/service/
 cp etc/motd /etc/motd
 
-apt-get install daemontools-run
