5cb468aba5685b8e75ef15156f2540752c9ca994
EWindowAdmin <build@ewindow.org>
2017-04-19 22:59:16 +0000

    Update list of known nodes

--- curses-ui/call.sh
+++ curses-ui/call.sh
@@ -2,17 +2,12 @@
 
 cd $(dirname $0) # Change working directory
 
-# Stop Services, in case the previous dialog exited in error while displaying
-#svc -d service/recv service/send
+# List of known nodes:
 
 CHOICE=$(dialog --menu "$(hostname)" 20 100 10 \
-	ewindow@exit.ewindow.org "Strfry's exit node" \
-	lorenza@debianwindow.ewindow.org "Lorenzas Laptop-Window" \
-	strfry@exit.ewindow.org	"strfry's Laptop" \
-	ewindow@dresden.ewindow.org	"RosenWerk Dresden" \
-	mumalab.ewindow.org	 "Munich Maker Lab" \
-	fablab-munich.ewindow.org	"FabLab Munich" \
-	exodus.strfry.org	"Gateway (not Available)" \
+	exit.ewindow.org "Strfry's exit node" \
+	testvm.ewindow.org "Build Virtual Machine" \
+	dresden.ewindow.org "RosenWERK Window"
 	Exit "" \
 	3>&1 1>&2 2>&3 )
 #	zockers.ewindow.org	"Call Zockers E-window" \
@@ -27,7 +22,7 @@ case $CHOICE in
 	*)
 		clear
 		#baresip -6 -v -e d"$CHOICE"
-		echo "d$CHOICE" | nc localhost 5555
+		echo "dewindow@$CHOICE" | nc localhost 5555
 		;;
 esac
 
