eba75772a74b49363ff04cafcc62d5f8bfac446d
Jonathan <ewindow@ewindow.org>
2017-04-19 23:49:47 +0000

    Move install location to /opt/ewindow

--- fetch-src.sh
+++ fetch-src.sh
@@ -3,13 +3,13 @@
 cd "$(dirname $0)"
 
 mkdir -p src
+pushd src
 
 # Install PeerVPN
 
-git clone https://github.com/strfry/ewindow
 git clone https://github.com/peervpn/peervpn
 git clone https://github.com/eleKtronicwindow/baresip
 git clone https://github.com/creytiv/re
 git clone https://github.com/creytiv/rem
 
-# Move source inside rootfs
+popd
--- setup-ewindow-user.sh
+++ setup-ewindow-user.sh
@@ -3,6 +3,10 @@
 # This script is intended for Raspbian
 # It creates the "ewindow" user with auto-login
 
+cd "$(dirname $0)"
+
+EPATH=/opt/ewindow
+
 if [ $(whoami) == "root" ]; then
   echo Am Root
 else
@@ -10,22 +14,25 @@ else
   exit 23
 fi
 
-cd "$(dirname $0)"
-
 echo Setting up ewindow user for autologin into curses-ui
 
 systemctl set-default multi-user.target
 ln -sf /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
 sed -i -e "s/--autologin pi/--autologin ewindow/g" /etc/systemd/system/autologin@.service
 
-useradd -G video,audio,netdev,sudo -s /home/ewindow/login.sh ewindow
+useradd -G video,audio,netdev,sudo,pi -s $ETPATH/curses-ui/login.sh -m ewindow
 ln -s "$PWD"/curses-ui /home/ewindow
-chown -R ewindow:ewindow /home/ewindow/
+
+# Fix up permissions...
+chown -R ewindow:ewindow $EPATH
+chmod -R g+rwX $EPATH
+
+
 echo Setting ewindow user password to 'ewindow'
-echo -en "ewindow\newindow" | passwd ewindow 2>/dev/null
+echo -en "ewindow\newindow" | passwd ewindow 2>&1 > /dev/null
 
-# Set up daemontools autorun:
-
+echo Set up daemontools autorun:
+set -x
 ln -sf "$PWD"/configs/peervpn /etc/service/
 ln -sf "$PWD"/configs/baresip /etc/service/
 cp configs/etc/motd /etc/motd
