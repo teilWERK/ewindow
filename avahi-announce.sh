9750d1e012d474c67bb0b7b45100229306ab8640
Jon <mail@strfry.org>
2017-05-13 19:15:30 +0200

    stuff

--- avahi-announce.sh
+++ avahi-announce.sh
@@ -1,3 +1,3 @@
-DESC="EWindow"
+DESC="EWindow $(hostname)"
 
 avahi-publish -s "$DESC" _sipuri._udp 5060 sip:ewindow@$(hostname).ewindow.org
--- avahi-discover.sh
+++ avahi-discover.sh
@@ -1,1 +1,9 @@
-avahi-browse -rpt _sipuri._udp | grep tap
+
+function filter() {
+   while read data; do
+      echo "$data"
+   done
+}
+
+#avahi-browse -rpt _sipuri._udp | grep = | grep IPv4 | grep tap0 | cut -d \; -f 10
+avahi-browse -art --no-db-lookup | grep tap0  | filter
