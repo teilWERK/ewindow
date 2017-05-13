df35caefc8b1ddb0aeb19796af521839599c2783
Your Mom <mail@strfry.org>
2017-05-13 17:13:02 +0000

    Use GStreamer/H264

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,9 @@
 apt install -y libssl-dev \
 	libasound-dev \
 	libvpx-dev libopus-dev \
+	libgstreamer1.0-dev \
+	libgstreamer-plugins-base1.0-dev \
+	libgstreamer-plugins-bad1.0-dev \
+	libavcodec-dev \
 	daemontools-run dialog
 
