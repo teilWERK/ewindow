b8940a56c1e0b8fe88b81ed56d683a2dccecef42
Jonathan <mail@strfry.org>
2017-06-15 20:59:14 +0000

    fetch-src.sh -> fetch-src-and-build.sh
    Simple build script

--- fetch-src-and-build.sh
+++ fetch-src-and-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 cd "$(dirname $0)"
 
@@ -11,5 +11,10 @@ git clone https://github.com/peervpn/peervpn
 git clone https://github.com/eleKtronicwindow/baresip
 git clone https://github.com/creytiv/re
 git clone https://github.com/creytiv/rem
+
+cd peervpn; make -j4 && sudo make install; cd ..
+cd re; make -j4 && sudo make install; cd ..
+cd rem; make -j4 && sudo make install; cd ..
+cd baresip; make -j4 && sudo make install; cd ..
 
 popd
