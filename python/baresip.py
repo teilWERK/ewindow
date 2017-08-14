bc06af3f79e07727b89281ffcf18ad66fc4be1b0
JonathanS <mail@strfry.org>
2017-08-14 23:57:03 +0200

    Automatically connect to only nodes, manage presence state

--- python/baresip.py
+++ python/baresip.py
@@ -3,13 +3,6 @@ from enum import Enum, IntEnum
 #import ctypes
 from ctypes import *
 
-bs = cdll.LoadLibrary('libbaresip.so')
-
-signal_h = CFUNCTYPE(None, c_int)
-#typedef void (ua_event_h)(struct ua *ua, enum ua_event ev,
-#			  struct call *call, const char *prm, void *arg);
-ua_event_h = CFUNCTYPE(None, c_void_p, c_int, c_void_p, c_char_p, c_void_p)
-
 class UA_EVENT(Enum):
 	UA_EVENT_REGISTERING 		= 0
 	UA_EVENT_REGISTER_OK		= 1
@@ -33,11 +26,6 @@ class PRESENCE_STATUS(Enum):
 	PRESENCE_CLOSED = 2
 	PRESENCE_BUSY = 3
 
-
-bs.conf_config.restype = c_void_p
-bs.uag_current.restype = c_void_p
-bs.baresip_contacts.restype = c_void_p
-
 class LE(Structure):
 	_fields_ = [("prev", c_void_p),
 				("next", c_void_p),
@@ -49,6 +37,7 @@ class PL(Structure):
 				("l", c_size_t)]
 
 	def __str__(self):
+		if not self.p: return "???"
 		return self.p[:self.l]
 
 class URI(Structure):
@@ -68,37 +57,59 @@ class SIP_ADDR(Structure):
 				("params", PL)
 	]
 
+signal_h = CFUNCTYPE(None, c_int)
+ua_event_h = CFUNCTYPE(None, c_void_p, c_int, c_void_p, c_char_p, c_void_p)
 
-bs.contact_list.restype = c_void_p
-bs.contact_list.argtypes = [c_void_p]
+def create_wrapper():
+	bs = cdll.LoadLibrary('libbaresip.so')
 
-bs.list_head.restype = POINTER(LE)
-bs.list_head.argtypes = [c_void_p]
+	bs.conf_config.restype = c_void_p
+	bs.uag_current.restype = c_void_p
+	bs.baresip_contacts.restype = c_void_p
 
-bs.baresip_init.argtypes = [c_void_p, c_bool]
+	bs.contact_list.restype = c_void_p
+	bs.contact_list.argtypes = [c_void_p]
 
-bs.contact_presence.argtypes = [c_void_p]
-bs.contact_presence.restype = PRESENCE_STATUS
+	bs.list_head.restype = POINTER(LE)
+	bs.list_head.argtypes = [c_void_p]
 
-bs.contact_addr.argtypes = [c_void_p]
-bs.contact_addr.restype = POINTER(SIP_ADDR)
+	bs.baresip_init.argtypes = [c_void_p, c_bool]
 
+	bs.contact_presence.argtypes = [c_void_p]
+	bs.contact_presence.restype = PRESENCE_STATUS
+
+	bs.contact_addr.argtypes = [c_void_p]
+	bs.contact_addr.restype = POINTER(SIP_ADDR)
+
+	bs.ua_presence_status_set.argtypes = [c_void_p, c_int]
+
+	bs.ua_hold_answer.argtypes = [c_void_p, c_void_p]
+	bs.ua_hangup.argtypes = [c_void_p, c_void_p, c_int16, c_char_p]
+
+	bs.notifier_update_status.argtype = [c_void_p]
+
+	bs.log_enable_debug.argtype = [c_bool]
+
+	return bs
+
+
+## Request Echo Cancellation for Pulseaudio:
+import os
+os.environ["PULSE_PROP"] = "filter.want=echo-cancel"
 
 ## START
 
+bs = create_wrapper()
 bs.libre_init()
 bs.conf_path_set("/home/strfry/.ewindow")
 bs.conf_configure()
+bs.log_enable_debug(True)
 
 prefer_ipv6 = True
 bs.baresip_init(bs.conf_config(), prefer_ipv6)
 bs.ua_init("EWindow", True, True, True, prefer_ipv6)
 bs.conf_modules()
 
-def event_handler(ua, event, call, prm, arg):
-	print "UAG EVENT!!!"
-	event = UA_EVENT(event)
-	print hex(ua), event, call, prm, arg
 
 def signal_handler(signal):
 	print "Signal: ", signal
@@ -106,45 +117,112 @@ def signal_handler(signal):
 	#exit()
 	if signal < 15: exit()
 
-evh = ua_event_h(event_handler)
-bs.uag_event_register(evh, 0x1337)
-
 def list_contacts():
 	cs = bs.baresip_contacts()
 	cl = bs.contact_list(cs)
 
 	h = bs.list_head(cl)
 
+	ret = []
+
 	while h:
 		c = h.contents.data
 		a = bs.contact_addr(c)
-		uri = "%s@%s" %(a.contents.uri.user, a.contents.uri.host)
+		#uri = "%s@%s" %(a.contents.uri.user, a.contents.uri.host)
+		uri = str(a.contents.auri)
+		
 		presence = bs.contact_presence(c)
-		print a.contents.dname, uri, presence
+		#print a.contents.dname, uri, presence
+		ret += [(a.contents.dname, uri, presence)]
 		
 		h = cast(h.contents.next, POINTER(LE))
 
-class AutoconnectManager(object):
-	def __init__(self):
-		current_connection = None
+	for x, y, z in ret: print x, y, z
 
-	def uag_callback(self, foo):
-		pass
+	return ret
+
+
+class ConnectionManager(object):
+	def __init__(self):
+		self.current_connection = None
+
+		# some ctypes magic to wrap self into void* arg
+		arg = cast(pointer(py_object(self)), POINTER(py_object))
+		self._py_callback = ua_event_h(self.uag_callback)
+		bs.uag_event_register(self._py_callback, arg)
+
+		set_presence_status(PRESENCE_STATUS.PRESENCE_OPEN)
+
+	@staticmethod
+	def uag_callback(ua, event, call, prm, arg):
+		self = cast(arg, POINTER(py_object)).contents.value
+		print "ConnectionManager: ", ua, event, call, prm
+
+		event = UA_EVENT(event)
+
+		if event == UA_EVENT.UA_EVENT_CALL_ESTABLISHED:
+			print "Call established"
+			set_presence_status(PRESENCE_STATUS.PRESENCE_BUSY)
+			self.current_connection = ("foo", "bar")
+		elif event == UA_EVENT.UA_EVENT_CALL_CLOSED:
+			print "Call closed, reason: ", prm
+			# Re-connect in case of rtp error
+			if "rtp error" in prm:
+				bs.ua_connect(bs.uag_current(), 0, 0, uri, 0, 1)
+				pass # Reconnect
+			else:
+				# Accept it
+				self.current_connection = None
+			set_presence_status(PRESENCE_STATUS.PRESENCE_OPEN)
+		elif event == UA_EVENT.UA_EVENT_CALL_INCOMING:
+			print "Incoming call"
+			if self.current_connection:
+				# TODO: do not deny if call comes from current connection 
+				# Present GUI
+				bs.ua_hangup(ua, call, 0, "Busy")
+			else:
+				bs.ua_hold_answer(ua, call)
+		else :
+			print "Unknown Event!!!", event
 
 	def manage(self):
-		if current_connection:
-			pass
+		if self.current_connection:
+			return
 		# Poll connections
 
-#call_uri = "lorenza@debianwindow.ewindow.org"
-call_uri = "pi@[fde1:c0fe::7]"
-bs.ua_connect(bs.uag_current(), 0, "sip:blablarg", call_uri, 0, 1)
+		candidates = []
+
+		for dname, uri, status in list_contacts():
+			if status == PRESENCE_STATUS.PRESENCE_OPEN:
+				print "Found connection candidate: ", dname
+				candidates += [(dname, uri)]
+
+		if not len(candidates): return
+
+		# Select random candidate from list
+		import random
+		idx = random.randint(0, len(candidates) - 1)
+		print "candidates", candidates, "idx", idx
+		dname, uri = candidates[idx]
+		print "Connecting to ", uri, "..."
+		self.current_connection = uri
+		bs.ua_connect(bs.uag_current(), 0, 0, uri, 0, 1)
+
+def set_presence_status(status):
+	if isinstance(status, PRESENCE_STATUS):
+		status = status.value
+	bs.ua_presence_status_set(bs.uag_current(), status)
+	bs.notifier_update_status(bs.uag_current())
+
 
 import thread
 import time
 
+connman = ConnectionManager()
+
 th = thread.start_new_thread (bs.re_main, (signal_h(signal_handler),))
 while True:
 	time.sleep(1)
-	list_contacts()
-#bs.re_main(0)
+	connman.manage()
+	#for d, u, s in list_contacts():
+	#	print d, s
